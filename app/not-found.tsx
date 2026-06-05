77313b8b55372d6d52f3e12473399729d094092c
NBhey <122431282+NBhey@users.noreply.github.com>
2026-06-05 21:52:54 +0300

    feat: добавить страницу not found

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound() {
+  return (
+    <div className="flex-1 self-center">
+      <p className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 lg:-translate-y-75 text-[192px] text-primary opacity-10">
+        404
+      </p>
+      <p>Could not find requested resource</p>
+    </div>
+  )
+}
