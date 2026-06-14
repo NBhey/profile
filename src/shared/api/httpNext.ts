523a684ef11c0c940fdeac8e8b49333538b0ba59
NBhey <122431282+NBhey@users.noreply.github.com>
2026-06-14 11:22:27 +0300

    feat: добавить get запрос

--- src/shared/api/httpNext.ts
+++ src/shared/api/httpNext.ts
@@ -9,6 +9,9 @@ const api = {
   post: <T>(url: string, data?: T, config?: InternalAxiosRequestConfig) => {
     return instance.post<T>(url, data, config)
   },
+  get: (url: string) => {
+    return instance.get(url)
+  },
 }
 
 export default api
