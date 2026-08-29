d8c2373749d3dab3c7992c532257085a9f069046
NBhey <122431282+NBhey@users.noreply.github.com>
2026-08-29 22:20:46 +0300

    feat: добавить navbar вниз

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from 'next'
 
 import './globals.css'
-import { Sidebar } from '@/src/widgets/home/ui/Sidebar'
+import { Header, Sidebar } from '@/src/widgets/home/ui/Sidebar'
 import { inter } from '@/app/fonts'
 import { baseMetadata } from '@/src/shared/config/seo'
-import type { ReactNode } from 'react'
+import { ReactNode } from 'react'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import UniqueVisitorTracking from '@/app/(analytics)/UniqueVisitorTracking'
+import { Navbar } from '@/src/shared/ui/Navbar/Navbar'
 
 export const metadata: Metadata = baseMetadata
 
@@ -18,11 +19,9 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="ru" className={`${inter.className} antialiased h-screen`}>
-      <body className="lg:p-3 md:min-h-full flex bg-background">
-        <div className="lg:p-4 m-auto relative max-w-300 w-full flex flex-col lg:h-151.25 md:flex-row md:border border-[#C7C4D7] rounded-lg">
-          <Sidebar />
-          {children}
-        </div>
+      <body>
+        {children}
+        <Navbar />
         <ToastContainer />
       </body>
 
