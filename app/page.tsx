04f0c6a261e46f786b4cc4c1080ced3ea542b9f5
NBhey <122431282+NBhey@users.noreply.github.com>
2026-08-30 17:36:56 +0300

    feat: добавить компонент автора

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from 'next'
 import { Typography } from '@/src/shared/ui/Typography/Typography'
 import { Button } from '@/src/shared/ui/Button/Button'
 import { BUTTON_VIEW } from '@/src/shared/model/types'
+import Image from 'next/image'
+import { Author } from '@/src/widgets/home/ui/Author'
 
 export const metadata: Metadata = {
   description:
@@ -10,45 +12,55 @@ export const metadata: Metadata = {
   alternates: { canonical: '/' },
 }
 
+// export default function Home() {
+//   return (
+//     <div className="gap-y-10 flex-1 flex flex-col h-full">
+//       <Typography as="h1" variant="bold" size="32-48">
+//         Привет, я Оганес
+//         <br className="none" /> Я разрабатываю{' '}
+//         <Typography
+//           as="span"
+//           variant="bold"
+//           size="32-48"
+//           className="text-primary italic"
+//         >
+//           быстрые
+//         </Typography>{' '}
+//         и современные веб-интерфейсы.
+//       </Typography>
+//       <hr />
+//       <Typography as="p" variant="normal" size="16-18">
+//         Специализируюсь на создании качественных фронтенд-решений, используя{' '}
+//         <b>NextJS</b>, <b>Tailwind</b>, <b>Python</b> и современные стандарты
+//         веб-разработки. <br /> Моя цель — писать чистый, поддерживаемый код и
+//         превращать сложные макеты в производительные и доступные интерфейсы.
+//       </Typography>
+//       <hr />
+//       <div className="flex justify-center gap-x-4">
+//         <Button href="experience" as={BUTTON_VIEW['LINK']} btnStyle="primary">
+//           Смотреть опыт{' '}
+//         </Button>
+//
+//         <Button href="contacts" as={BUTTON_VIEW['LINK']} btnStyle="outlined">
+//           Связаться
+//         </Button>
+//       </div>
+//
+//       {/*<div className="pt-12 flex flex-col gap-y-6 border-t border-[#C7C4D7]">*/}
+//       {/*  <Typography as="h3" variant="bold">*/}
+//       {/*    Техническая полка*/}
+//       {/*  </Typography>*/}
+//       {/*</div>*/}
+//     </div>
+//   )
+// }
+
 export default function Home() {
   return (
-    <div className="gap-y-10 flex-1 flex flex-col h-full justify-evenly">
-      <Typography as="h1" variant="bold" size="32-48">
-        Привет, я Оганес
-        <br className="none" /> Я разрабатываю{' '}
-        <Typography
-          as="span"
-          variant="bold"
-          size="32-48"
-          className="text-primary italic"
-        >
-          быстрые
-        </Typography>{' '}
-        и современные веб-интерфейсы.
-      </Typography>
-      <hr />
-      <Typography as="p" variant="normal" size="16-18">
-        Специализируюсь на создании качественных фронтенд-решений, используя{' '}
-        <b>NextJS</b>, <b>Tailwind</b>, <b>Python</b> и современные стандарты
-        веб-разработки. <br /> Моя цель — писать чистый, поддерживаемый код и
-        превращать сложные макеты в производительные и доступные интерфейсы.
-      </Typography>
-      <hr />
-      <div className="flex gap-x-4">
-        <Button href="experience" as={BUTTON_VIEW['LINK']} btnStyle="primary">
-          Смотреть опыт{' '}
-        </Button>
-
-        <Button href="contacts" as={BUTTON_VIEW['LINK']} btnStyle="outlined">
-          Связаться
-        </Button>
+    <main>
+      <div className="py-2 px-3 flex justify-between border-b border-gray-200">
+        <Author />
       </div>
-
-      {/*<div className="pt-12 flex flex-col gap-y-6 border-t border-[#C7C4D7]">*/}
-      {/*  <Typography as="h3" variant="bold">*/}
-      {/*    Техническая полка*/}
-      {/*  </Typography>*/}
-      {/*</div>*/}
-    </div>
+    </main>
   )
 }
--- src/widgets/home/ui/Author.tsx
+++ src/widgets/home/ui/Author.tsx
@@ -1,34 +1,33 @@
 'use client'
 
-import { usePathname } from 'next/navigation'
 import Image from 'next/image'
-import { formatDate } from '@/src/shared/lib/formatDate'
 import { Typography } from '@/src/shared/ui/Typography/Typography'
 
 export const Author = () => {
-  const pathname = usePathname()
   return (
     <>
-      {pathname === '/' && (
-        <div className="flex flex-col lg:flex-row gap-x-3 py-8 border-t border-[#C7C4D7] lg:animate-fade-slide-up">
-          <Image
-            src="/assets/author.png"
-            alt="author"
-            width={50}
-            height={50}
-            style={{ borderRadius: '50%', border: '1px solid #C7C4D7' }}
-          />
+      <div className=" flex flex-col gap-2">
+        <Typography as="h1" variant="bold" size="16-18">
+          Оганес М.-Т.
+        </Typography>
 
-          <div className="flex flex-col">
-            <Typography as="h3" variant="normal" size="12-14">
-              Оганес Маркарян-Тридрих
-            </Typography>
-            <Typography as="h4" variant="normal" size="12-14">
-              Опыт коммерческой разработки {formatDate(new Date())}
-            </Typography>
-          </div>
-        </div>
-      )}
+        <Typography
+          as="h3"
+          variant="normal"
+          size="12-14"
+          className="uppercase text-secondary tracking-[5px]"
+        >
+          Frontend разработчик
+        </Typography>
+      </div>
+      <Image
+        src="/assets/author.png"
+        alt="author"
+        width={50}
+        height={50}
+        style={{ borderRadius: '50%', border: '1px solid #C7C4D7' }}
+        loading="eager"
+      />
     </>
   )
 }
