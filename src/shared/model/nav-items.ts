2ac42eccd639d054df349281fca3874efe5ca8d8
NBhey <122431282+NBhey@users.noreply.github.com>
2026-09-12 21:44:20 +0300

    refactor: переименовать имя в навигационной панели

--- src/shared/model/nav-items.ts
+++ src/shared/model/nav-items.ts
@@ -2,12 +2,12 @@ import { ROUTES } from '@/src/shared/config/routes'
 
 export const NAV_ROUT_ITEMS = [
   { href: ROUTES.home, name: 'home', title: 'Главная' },
-  { href: ROUTES.experience, name: 'experience', title: 'Опыт работы' },
+  { href: ROUTES.experience, name: 'experience', title: 'Опыт' },
   { href: ROUTES.projects, name: 'projects', title: 'Проекты' },
   {
     href: ROUTES.otherWorkExperience,
     name: 'otherWorkExperience',
-    title: 'Опыт вне it',
+    title: 'Вне it',
   },
   { href: ROUTES.contacts, name: 'contacts', title: 'Контакты' },
 ]
